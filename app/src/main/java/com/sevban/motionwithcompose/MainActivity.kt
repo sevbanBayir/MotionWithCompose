f8525f7eb1fbbd87575880346935f395a20a2c90
SevbanBayir <sevbanbuyer@gmail.com>
2022-07-24 18:06:39 +0300

    finished.

--- app/src/main/java/com/sevban/motionwithcompose/MainActivity.kt
+++ app/src/main/java/com/sevban/motionwithcompose/MainActivity.kt
@@ -3,13 +3,29 @@ package com.sevban.motionwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import androidx.constraintlayout.compose.layoutId
 import com.sevban.motionwithcompose.ui.theme.MotionWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +34,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             MotionWithComposeTheme {
 
+                Column{
+                    var progress by remember { mutableStateOf(0f) }
+
+                    ProfileHeader(progress = progress)
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    Slider(
+                        value = progress,
+                        onValueChange = { progress = it },
+                        modifier = Modifier.padding(horizontal = 32.dp)
+                    )
+                }
 
             }
         }
     }
+}
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun ProfileHeader(progress: Float) {
+    val context= LocalContext.current
+    val motionScene= remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val properties = motionProperties(id = "profile_pic")
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.DarkGray)
+            .layoutId("box")
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "pp",
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = properties.value.color("background"),
+                    shape = CircleShape
+                )
+                .layoutId("profile_pic")
+        )
+        Text(
+            text = "Sevban Bayır",
+            fontSize = 24.sp,
+            modifier = Modifier.layoutId("username"),
+            color = properties.value.color("background")
+        )
+    }
+
 }
 
